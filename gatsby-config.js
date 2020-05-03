fb9b2dc7861346b955bad67cb30a95a2b5d81c69
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-05-03 23:30:05 +0200

    added Navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: "/portfolio",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gérald Devreux`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,42 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Nav, Navbar, NavbarBrand, NavItem } from 'react-bootstrap'
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header>
+    <Navbar>
+      <NavbarBrand href="/">
+        {siteTitle}
+      </NavbarBrand>
+      <NavbarToggle aria-controls="navbarResponsive" />
+      <NavbarCollapse id="navbarResponsive">
+        <Nav as="ul">
+          <NavItem as="li">
+            <Link to="/page-2" className="nav-link" activeClassName="active">
+              Page 2
+            </Link>
+          </NavItem>
+          <NavItem as="li">
+            <Link to="/page-2" className="nav-link" activeClassName="active">
+            Profile
+            </Link>
+          </NavItem>
+          <NavItem as="li">
+            <Link to="/page-2" className="nav-link" activeClassName="active">
+            Skills
+            </Link>
+          </NavItem>
+          <NavItem as="li">
+            <Link to="/page-2" className="nav-link" activeClassName="active">
+              Onglet 4
+            </Link>
+          </NavItem>
+        </Nav>
+      </NavbarCollapse>
+    </Navbar>
   </header>
 )
 
