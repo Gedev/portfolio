d1936db1e47bb5567fc14fbc9cee4024a1c7f141
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-05-22 00:50:54 +0200

    test push with new ssh key

--- src/components/body/contact.js
+++ src/components/body/contact.js
@@ -1,27 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Form, FormLabel } from "react-bootstrap"
+import { Button, Form, FormGroup, FormLabel } from "react-bootstrap"
 const Contact = () => (
     <section id="contact" className="blockA">
       <h2>Contact</h2>
       <Form method="post" action="#">
-        <FormLabel>
-          Name
-          <input type="text" name="name" id="name" />
-        </FormLabel>
-        <FormLabel>
-          Email
-          <input type="email" name="email" id="email" />
-        </FormLabel>
-        <FormLabel>
-          Subject
-          <input type="text" name="subject" id="subject" />
-        </FormLabel>
-        <FormLabel>
-          Message
-          <textarea name="message" id="message" rows="5" />
-        </FormLabel>
+
+        <FormGroup>
+          <FormLabel>
+            Name
+            <input type="text" name="name" id="name" />
+          </FormLabel>
+        </FormGroup>
+
+        <FormGroup>
+          <FormLabel>
+            Email
+            <input type="email" name="email" id="email" />
+          </FormLabel>
+        </FormGroup>
+        <FormGroup>
+          <FormLabel>
+            Subject
+            <input type="text" name="subject" id="subject" />
+          </FormLabel>
+        </FormGroup>
+        <FormGroup>
+          <FormLabel>
+            Message
+            <textarea name="message" id="message" rows="5" />
+          </FormLabel>
+        </FormGroup>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
+
         <Link to="/">Go Up 🔼</Link>
     </section>
 )
