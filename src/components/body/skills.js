fc92077b3e929980ec07eb40e3c98c6f82ac16db
Gedev <gerald.devreux.pro@gmail.com>
2020-05-27 23:40:30 +0200

    add skills informations

--- src/components/body/skills.js
+++ src/components/body/skills.js
@@ -4,6 +4,13 @@ const Skills = () => (
     <section id="skills" className="blockA">
         <div>
             <h2>Skills</h2>
+            <span>HTML 5</span>
+            <span>CSS 3</span>
+            <span>Javascript</span>
+            <h4>Frameworks</h4>
+            <span>Laravel</span>
+            <span>Symfony</span>
+            <span>React</span>
         </div>
     </section>
 )
