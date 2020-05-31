25deeaf52479eb13bda616d48f906cfe49efd7f0
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-05-31 23:57:36 +0200

    few things to do in side projects

--- src/components/body/side-projects.js
+++ src/components/body/side-projects.js
@@ -4,6 +4,8 @@ const SideProjects = () => (
   <section id="side-projects" className="blockB">
     <div>
       <h2>Side Projects</h2>
+      *Waiting for current meetups*
+      *Waiting for technology watch module*
     </div>
   </section>
 )
