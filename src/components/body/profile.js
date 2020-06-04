b66f7c56c24bb53bc61acd78d06fde9b21443c89
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-06-04 23:28:21 +0200

    add cards react bootstrap to the profile section

--- src/components/body/profile.js
+++ src/components/body/profile.js
@@ -1,42 +1,52 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Profile = () => (
     <section className="blockB">
         <div id="profile">
-            <h2>Profile</h2>
-            <h3>EXPERIENCE</h3>
-            <p>
-            Stagiaire en développement Web - HommeDeMetierBE
-            2020 - 2,5 mois
-            Création des plateformes web pour le site HommeDeMetier sous la méthode Agile/SCRUM.
-            </p>
-            <p>
-            Technologies :
-            HTML / JS / CSS
-            React/Rematch/Redux
-            Symfony
-            Docker
-            </p>
-            <p>
-            <h6>
-            Outils :
-            </h6>
-            Hubspot API
-            Bitbucket
-            Clubhouse
-            Slack
-            Git
-            </p>
-            <h6>
-            Méthodes de gestion de travail :
-            </h6>
-            Méthode Agile/SCRUM
-            Peercoding
-            Game Development - Technobel
-            2016 - 3 mois
-            Développement de jeu sur navigateur en JS. Algorithmique, game engine, POO.
-            Finalisation des compétences dans un jeu présenté devant le studio Fishing Cactus.
-
+          <h2>Profile</h2>
+          <h3>EXPERIENCE</h3>
+            <Card>
+              <Card.Header>
+                <h5>
+                Stagiaire en développement Web - HommeDeMetierBE
+                2020 - 2,5 mois
+                Création des plateformes web pour le site HommeDeMetier sous la méthode Agile/SCRUM.
+                </h5>
+              </Card.Header>
+              <Card.Body className="cardBody">
+                <Card.Title>Technologies :</Card.Title>
+                <Card.Text>
+                  <p>
+                    HTML / JS / CSS / React / Rematch / Redux / Symfony / Docker
+                  </p>
+                  <p>
+                    <Card.Title>Outils :</Card.Title>
+                    Hubspot API
+                    Bitbucket
+                    Clubhouse
+                    Slack
+                    Git
+                  </p>
+                  <Card.Title>Méthodes de gestion de travail :</Card.Title>
+                  Méthode Agile/SCRUM
+                  Peercoding
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          <Card>
+              <Card.Header>
+                <h5>Game Development - Technobel
+                  2016 - 3 mois</h5>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>Description</Card.Title>
+                <Card.Text>
+                  Développement de jeu sur navigateur en JS. Algorithmique, game engine, POO.
+                  Finalisation des compétences dans un jeu présenté devant le studio Fishing Cactus.
+                </Card.Text>
+              </Card.Body>
+            </Card>
 
             <h4>FORMATIONS</h4>
             Web Development (3ème) — IEPSCF
