77a7256579005767fca7dbb42216cdfe6eeb30af
Gedev <gerald.devreux.pro@gmail.com>
2020-06-07 18:43:05 +0200

    remove sal animations from contact section

--- src/components/body/contact.js
+++ src/components/body/contact.js
@@ -1,45 +1,53 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Button, Form, FormGroup, FormLabel } from "react-bootstrap"
+import {Button, Card, Form, FormGroup, FormLabel} from "react-bootstrap"
 const Contact = () => (
     <section id="contact" className="blockA">
       <h2>Contact</h2>
-      <Form method="post" action="#">
 
-        <div data-sal="slide-up"
-             data-sal-delay="300"
-             data-sal-easing="ease">
-        <FormGroup data-sal="slide-up"
-                   data-sal-delay="300"
-                   data-sal-easing="ease">
+
+      <Form method="post" action="#">
+        <div>
+        <FormGroup>
           <FormLabel>
             Name
-            <input type="text" name="name" id="name" />
           </FormLabel>
+          <input type="text" name="name" id="name" />
         </FormGroup>
         </div>
 
+        <div>
         <FormGroup>
           <FormLabel>
             Email
-            <input type="email" name="email" id="email" />
           </FormLabel>
+          <input type="email" name="email" id="email"/>
         </FormGroup>
+        </div>
+
+        <div>
         <FormGroup>
           <FormLabel>
             Subject
             <input type="text" name="subject" id="subject" />
           </FormLabel>
         </FormGroup>
+        </div>
+
+        <div>
         <FormGroup>
           <FormLabel>
             Message
             <textarea name="message" id="message" rows="5" />
           </FormLabel>
         </FormGroup>
+        </div>
+
+        <div>
         <Button variant="primary" type="submit">
           Submit
         </Button>
+        </div>
       </Form>
 
         <Link to="/">Go Up 🔼</Link>
