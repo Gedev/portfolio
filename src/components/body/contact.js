765dfcc717c06505b70273df13cbc545f92cefc1
Gedev <gerald.devreux.pro@gmail.com>
2020-06-14 23:16:54 +0200

    clean the form and made it operational with getform.io

--- src/components/body/contact.js
+++ src/components/body/contact.js
@@ -4,87 +4,73 @@ import { Col, Button, Form, FormGroup, FormLabel, Row} from "react-bootstrap"
 const Contact = () => (
     <section id="contact" className="blockA">
       <h2>Contact</h2>
-      {/*<Form method="post" action="#">*/}
+      <Form method="post" action="https://getform.io/f/343e9536-9a0e-4ce6-a1be-de1b83bb6b90">
 
 
-      {/*  <div data-sal="slide-up"*/}
-      {/*       data-sal-delay="1200"*/}
-      {/*       data-sal-easing="ease"*/}
-      {/*       data-sal-duration="1000">*/}
-      {/*  <FormGroup>*/}
+        <div data-sal="slide-up"
+             data-sal-delay="1200"
+             data-sal-easing="ease"
+             data-sal-duration="1000">
+        <FormGroup>
 
-      {/*    <Row>*/}
-      {/*      <Col>*/}
-      {/*        <FormLabel>*/}
-      {/*          Your firstname*/}
-      {/*        </FormLabel>*/}
-      {/*        <Form.Control placeholder="First name" />*/}
-      {/*      </Col>*/}
-      {/*      <Col>*/}
-      {/*        <FormLabel>*/}
-      {/*          Your name*/}
-      {/*        </FormLabel>*/}
-      {/*        <Form.Control placeholder="Last name" />*/}
-      {/*      </Col>*/}
-      {/*    </Row>*/}
-      {/*  </FormGroup>*/}
-      {/*  </div>*/}
+          <Row>
+            <Col>
+              <FormLabel>
+                Your firstname
+              </FormLabel>
+              <Form.Control type="text" name="name" placeholder="First name" />
+            </Col>
+            <Col>
+              <FormLabel>
+                Your name
+              </FormLabel>
+              <Form.Control placeholder="Last name" />
+            </Col>
+          </Row>
+        </FormGroup>
+        </div>
 
-      {/*  <div data-sal="slide-up"*/}
-      {/*       data-sal-delay="200"*/}
-      {/*       data-sal-easing="ease"*/}
-      {/*       data-sal-duration="1000">*/}
-      {/*  <FormGroup>*/}
-      {/*    <FormLabel>*/}
-      {/*      Email*/}
-      {/*    </FormLabel>*/}
-      {/*    <Form.Control type="email" placeholder="name@example.com" />*/}
-      {/*  </FormGroup>*/}
-      {/*  </div>*/}
+        <div data-sal="slide-up"
+             data-sal-delay="200"
+             data-sal-easing="ease"
+             data-sal-duration="1000">
+        <FormGroup>
+          <FormLabel>
+            Email
+          </FormLabel>
+          <Form.Control type="email" name="email" placeholder="name@example.com" />
+        </FormGroup>
+        </div>
 
-      {/*  <div data-sal="slide-up"*/}
-      {/*       data-sal-delay="400"*/}
-      {/*       data-sal-easing="ease"*/}
-      {/*       data-sal-duration="1000">*/}
-      {/*  <FormGroup>*/}
-      {/*    <FormLabel>*/}
-      {/*      Subject*/}
-      {/*    </FormLabel>*/}
-      {/*      <Form.Control type="text" placeholder="Subject" />*/}
+        <div data-sal="slide-up"
+             data-sal-delay="400"
+             data-sal-easing="ease"
+             data-sal-duration="1000">
+        <FormGroup>
+          <FormLabel>
+            Subject
+          </FormLabel>
+            <Form.Control type="text" placeholder="Subject" />
 
-      {/*  </FormGroup>*/}
-      {/*  </div>*/}
+        </FormGroup>
+        </div>
 
-      {/*  <div data-sal="slide-up"*/}
-      {/*       data-sal-delay="600"*/}
-      {/*       data-sal-easing="ease"*/}
-      {/*       data-sal-duration="1000">*/}
-      {/*  <FormGroup>*/}
-      {/*    <FormLabel>*/}
-      {/*      Message*/}
-      {/*      <Form.Control as="textarea" rows="5" placeholder="Your message" />*/}
-      {/*    </FormLabel>*/}
-      {/*  </FormGroup>*/}
-      {/*  </div>*/}
+        <div data-sal="slide-up"
+             data-sal-delay="600"
+             data-sal-easing="ease"
+             data-sal-duration="1000">
+        <FormGroup>
+          <FormLabel>
+            Message
+            <Form.Control as="textarea" name="message" rows="5" placeholder="Your message" />
+          </FormLabel>
+        </FormGroup>
+        </div>
 
-      {/*  <div data-sal="slide-up"*/}
-      {/*      data-sal-delay="800"*/}
-      {/*      data-sal-easing="ease"*/}
-      {/*      data-sal-duration="1000">*/}
-      {/*  <Button variant="primary" type="submit">*/}
-      {/*    Submit*/}
-      {/*  </Button>*/}
-      {/*  </div>*/}
-      {/*</Form>*/}
-
-      <form action="https://getform.io/f/343e9536-9a0e-4ce6-a1be-de1b83bb6b90" method="POST">
-
-        <input type="text" name="name" />
-          <input type="email" name="email" />
-            <input type="text" name="message" />
-              <button type="submit">Send</button>
-
-      </form>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
 
       <div className="go-up">
         <Link to="/">Go Up 🔼</Link>
