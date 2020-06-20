a719210a7635190aab729b1d2dc6569f7af47c55
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-06-20 22:37:19 +0200

    add import image

--- src/components/body/side-projects.js
+++ src/components/body/side-projects.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import Image from "../image";
 
 const SideProjects = () => (
   <section id="side-projects" className="blockB">
     <Container>
+
       <h2>Side Projects</h2>
       *Waiting for current meetups*
       *Waiting for technology watch module*
-    <h4>
-      <p>Actually improving my skills in :</p>
-    </h4>
+      <h4>
+        <p>Actually improving my skills in :</p>
+      </h4>
       <p> Latest article I read : https://medium.com/better-programming/functional-programming-using-the-functor-type-class-in-typescript-beae60abe6af</p>
+
+      <Card>
+      <Card.Header>
+        <h5>
+          Dark'n Light
+        </h5>
+      </Card.Header>
+      <Card.Body className="cardBody">
+        <Card.Title>Preview</Card.Title>
+        <Card.Text>
+          <img src={}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+
+
+
     </Container>
   </section>
 )
