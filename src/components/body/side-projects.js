7d7ac4592cea0b93113519154996c17fe1ead6cc
Gérald Devreux <gerald.devreux.pro@gmail.com>
2020-06-21 21:59:35 +0200

    added images for side projects previews

--- src/components/body/side-projects.js
+++ src/components/body/side-projects.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Card, Container } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import Image from "../image";
+
+import preview_dark_n_light from "../../images/preview_dark_n_light.png";
+import poulpatose from "../../images/poulpatose.jpg";
 
 const SideProjects = () => (
   <section id="side-projects" className="blockB">
@@ -14,19 +17,54 @@ const SideProjects = () => (
       </h4>
       <p> Latest article I read : https://medium.com/better-programming/functional-programming-using-the-functor-type-class-in-typescript-beae60abe6af</p>
 
+      <Row>
+      <Col>
       <Card>
       <Card.Header>
         <h5>
-          Dark'n Light
+          A javascript navigator game
         </h5>
       </Card.Header>
       <Card.Body className="cardBody">
         <Card.Title>Preview</Card.Title>
         <Card.Text>
-          <img src={}
+          <img src={preview_dark_n_light} alt="darknlightpreview" />
+          Dark'n Light, my personal game using the framework Phaser.
         </Card.Text>
       </Card.Body>
     </Card>
+      </Col>
+        <Col>
+          <Card>
+            <Card.Header>
+              <h5>
+                A funny Card game
+              </h5>
+            </Card.Header>
+            <Card.Body className="cardBody">
+              <Card.Title>Preview</Card.Title>
+              <Card.Text>
+                <img src={poulpatose} alt="darknlightpreview" />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Header>
+              <h5>
+                Project 3
+              </h5>
+            </Card.Header>
+            <Card.Body className="cardBody">
+              <Card.Title>Preview</Card.Title>
+              <Card.Text>
+
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
 
 
