8c074beffe11497fb978902ab13e07ad4356bfa7
Gérald Devreux <gerald.devreux.pro@gmail.com>
2021-04-24 00:56:52 +0200

    Remove unused icons, add technologies icons to side projects

--- src/components/body/side-projects.js
+++ src/components/body/side-projects.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import {Card, Col, Container, OverlayTrigger, Row, Tooltip} from "react-bootstrap";
 
 import preview_dark_n_light from "../../images/preview_dark_n_light.png";
 import poulpatose from "../../images/poulpatose.jpg";
 import announcements from "../../images/announcements.png";
 import classroomFinder_homepage from "../../images/classroomFinder_homepage.png";
 import classroomFinder_users from "../../images/classroomFinder_users.png";
+import phpLogo from "../../images/software-development-logos/png/027-php.png";
+import laravelLogo from "../../images/laravel-brands.svg";
+
+
+function renderTooltip(props) {
+  return (
+      <Tooltip id="button-tooltip" {...props}>
+        {props}
+      </Tooltip>
+  );
+}
 
 const SideProjects = () => (
   <section id="side-projects" className="blockB">
@@ -29,7 +40,23 @@ const SideProjects = () => (
                 <img src={classroomFinder_homepage} alt="classroomFinder_homepage_Preview" />
                 <img src={announcements} alt="announcementsPreview" />
                 <img src={classroomFinder_users} alt="classroomFinder_usersPreview" />
-                <p>Dark'n Light, my personal game using the framework Phaser.</p>
+                <Card.Title>Tech :</Card.Title>
+                <p className="logos">
+                  <OverlayTrigger
+                      placement="top"
+                      delay={{show: 0, hide: 0}}
+                      overlay={renderTooltip("PhP")}
+                  >
+                  <img src={phpLogo} alt="phpLogo"/>
+                  </OverlayTrigger>
+                  <OverlayTrigger
+                      placement="top"
+                      delay={{show: 0, hide: 0}}
+                      overlay={renderTooltip("Laravel")}
+                  >
+                  <img src={laravelLogo} alt="laravelLogo"/>
+                  </OverlayTrigger>
+                </p>
               </Card.Text>
             </Card.Body>
           </Card>
@@ -56,15 +83,14 @@ const SideProjects = () => (
           <Card>
             <Card.Header>
               <h5>
-                A funny Card game
+                Poulpatose
               </h5>
             </Card.Header>
             <Card.Body className="cardBody">
               <Card.Title>Preview</Card.Title>
               <Card.Text>
-                <img src={poulpatose} alt="darknlightpreview" />
-                <p>Poulpatose, the card game I created and printed.</p>
-
+                <img src={poulpatose} alt="poulpatosePreview" />
+                <p>A funny Card game I created and printed.</p>
               </Card.Text>
             </Card.Body>
           </Card>
